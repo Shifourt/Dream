27dac7d155064bd3987c246621d42ce1f57ff33c
Shifat Hossain <shif4t@gmail.com>
2022-03-19 22:50:44 +0600

    footer

--- src/components/Layouts/Footer/index.tsx
+++ src/components/Layouts/Footer/index.tsx
@@ -1,3 +1,11 @@
+import {
+  faFacebookF,
+  faGithub,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import React, { useEffect } from "react";
@@ -12,6 +20,7 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <div className={styles.Wrapper}>
       <div data-aos="fade-up" className={styles.footerCap}></div>
+
       <div className={styles.content}>
         <div className={styles.Section}>
           <div className={styles.innerContent}>
@@ -26,6 +35,48 @@ const Footer: React.FC<FooterProps> = () => {
                 <a href="">ad choices</a>
               </li>
             </ul>
+            <div className={styles.Social}>
+              <div className={styles.Icons}>
+                <div className={styles.tooltip}>Facebook</div>
+                <span>
+                  <div className={styles.facebook}>
+                    <FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon>
+                  </div>
+                </span>
+              </div>
+              <div className={styles.Icons}>
+                <div className={styles.tooltip}>Twitter</div>
+                <span>
+                  <div className={styles.twitter}>
+                    <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                  </div>
+                </span>
+              </div>
+              <div className={styles.Icons}>
+                <div className={styles.tooltip}>Instagram</div>
+                <span>
+                  <div className={styles.instagram}>
+                    <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                  </div>
+                </span>
+              </div>
+              <div className={styles.Icons}>
+                <div className={styles.tooltip}>Github</div>
+                <span>
+                  <div className={styles.github}>
+                    <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                  </div>
+                </span>
+              </div>
+              <div className={styles.Icons}>
+                <div className={styles.tooltip}>YouTube</div>
+                <span>
+                  <div className={styles.youtube}>
+                    <FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon>
+                  </div>
+                </span>
+              </div>
+            </div>
             <a
               className={styles.copyright}
               href="https://www.facebook.com/danted30"
