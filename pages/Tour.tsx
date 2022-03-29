ef99174265b15b8c9b03638f6393c8ef29c40bac
Shifat Hossain <shif4t@gmail.com>
2022-03-29 20:50:57 +0600

    nowFinal

--- pages/Tour.tsx
+++ pages/Tour.tsx
@@ -2,14 +2,12 @@ import type { NextPage } from "next";
 import Brake from "../src/components/homepage/Brake";
 import BrakeTwo from "../src/components/homepage/BrakeTwo";
 import Layouts from "../src/components/Layouts";
-import Tour from "../src/components/TourSpots";
 
 const Home: NextPage = (props) => {
   return (
     <Layouts>
       <Brake />
       <BrakeTwo />
-      <Tour />
     </Layouts>
   );
 };
